bbda24a80b9519e31c09e821f243fb9911c08ba6
root <root@ip-172-31-40-198.eu-west-1.compute.internal>
2017-05-03 20:23:02 +0000

    initial add

---
+++ __openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2017 Igor V. Kartashov
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": 'partner WEB Form',
+    "version": "9.0.1.0",
+    "summary": "extensión formulario WEB para los clientes con acceso a portal",
+    "description": """
+		i-vk
+		v9.0.1.0
+		extensión formulario WEB para los clientes con el acceso a portal
+	""",
+    "author": "Igor V. Kartashov",
+    "license": "AGPL-3",
+    "website": "http://crm.setir.es",
+    "category": "Partner",
+    "depends": ["base",
+				"partnerAssociate",
+				"website",
+				],
+    "data": [
+        "views/templates.xml",
+    ],
+    "installable": True,
+    "application": True,
+	"auto_install": False,
+    "price": 0.00,
+    "currency": "EUR"
+}
---
+++ controllers/main.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+#import email
+#import datetime
+from openerp import fields
+from openerp import http
+from openerp.http import request
+from openerp import tools
+from openerp.tools.translate import _
+from openerp.addons.website.controllers.main import Website
+from openerp.addons.website_portal.controllers.main import website_account
+
+
+
+class website_account(http.Controller):
+    @http.route(['/my', '/my/home'], type='http', auth="user", website=True)
+    def account(self, **kw):
+        partner = request.env.user.partner_id
+
+        # get customer sales rep
+        if partner.user_id:
+            sales_rep = partner.user_id
+        else:
+            sales_rep = False
+        values = {
+            'sales_rep': sales_rep,
+            'company': request.website.company_id,
+            'user': request.env.user
+        }
+
+        return request.website.render("website_portal.account", values)
+
+    @http.route(['/my/account'], type='http', auth='user', website=True)
+    def details(self, redirect=None, **post):
+        partner = request.env['res.users'].browse(request.uid).partner_id
+        values = {
+            'error': {},
+            'error_message': []
+        }
+
+        if post:
+            error, error_message = self.details_form_validate(post)
+            values.update({'error': error, 'error_message': error_message})
+            values.update(post)
+            if not error:
+                post.update({'zip': post.pop('zipcode', '')})
+                if partner.type == "contact":
+                    address_fields = {
+                        'city': post.pop('city'),
+                        'street': post.pop('street'),
+                        'street2': post.pop('street2'),
+                        'zip': post.pop('zip'),
+                        'country_id': post.pop('country_id'),
+                        'state_id': post.pop('state_id'),
+						'x_strAuthorizationTransport': post.pop('x_strAuthorizationTransport'),
+						'x_nAuthorizationTransport': post.pop('x_nAuthorizationTransport'),
+						'x_strLicenceEU': post.pop('x_strLicenceEU'),
+						'x_strAuthorizationOperator': post.pop('x_strAuthorizationOperator'),
+						'x_nEmplyees': post.pop('x_nEmplyees'),
+						'email': post.pop('company_email'),
+						'phone': post.pop('company_phone')
+                    }
+                    partner.commercial_partner_id.sudo().write(address_fields)
+                partner.sudo().write(post)
+                if redirect:
+                    return request.redirect(redirect)
+                return request.redirect('/my/home')
+
+        countries = request.env['res.country'].sudo().search([])
+        states = request.env['res.country.state'].sudo().search([])
+
+        values.update({
+			'company_name': partner.commercial_partner_id.name,
+			'company_email': partner.commercial_partner_id.email,
+			'company_phone': partner.commercial_partner_id.phone,
+			'x_strAuthorizationTransport': partner.commercial_partner_id.x_strAuthorizationTransport,
+			'x_nAuthorizationTransport':  partner.commercial_partner_id.x_nAuthorizationTransport,
+			'x_strLicenceEU': partner.commercial_partner_id.x_strLicenceEU,
+			'x_strAuthorizationOperator':  partner.commercial_partner_id.x_strAuthorizationOperator,
+			'x_nEmplyees': partner.commercial_partner_id.x_nEmplyees,
+            'partner': partner,
+            'countries': countries,
+            'states': states,
+            'has_check_vat': hasattr(request.env['res.partner'], 'check_vat'),
+            'redirect': redirect,
+        })
+
+        return request.website.render("website_portal.details", values)
+
+    def details_form_validate(self, data):
+        error = dict()
+        error_message = []
+
+        mandatory_billing_fields = ["name", "phone", "email", "street", "city", "country_id"]
+        optional_billing_fields = ["zipcode",
+									"state_id",
+									"vat",
+									"street2",
+									"x_strAuthorizationTransport",
+									"x_nAuthorizationTransport",
+									"x_strLicenceEU",
+									"x_strAuthorizationOperator",
+									"x_nEmplyees",
+									"company_name",
+									"company_email",
+									"company_phone"
+									]
+
+        # Validation
+        for field_name in mandatory_billing_fields:
+            if not data.get(field_name):
+                error[field_name] = 'missing'
+
+        # email validation
+        if data.get('email') and not tools.single_email_re.match(data.get('email')):
+            error["email"] = 'error'
+            error_message.append(_('Invalid Email! Please enter a valid email address.'))
+
+        if data.get('company_email') and not tools.single_email_re.match(data.get('company_email')):
+            error["company_email"] = 'error'
+            error_message.append(_('Invalid Email! Please enter a valid email address.'))
+
+        # vat validation
+        if data.get("vat") and hasattr(request.env["res.partner"], "check_vat"):
+            if request.website.company_id.vat_check_vies:
+                # force full VIES online check
+                check_func = request.env["res.partner"].vies_vat_check
+            else:
+                # quick and partial off-line checksum validation
+                check_func = request.env["res.partner"].simple_vat_check
+            vat_country, vat_number = request.env["res.partner"]._split_vat(data.get("vat"))
+            if not check_func(vat_country, vat_number):  # simple_vat_check
+                error["vat"] = 'error'
+                error_message.append(_('NIF no válido! Formtato válido: ESA12345678.'))
+
+		# error message for empty required fields
+        if [err for err in error.values() if err == 'missing']:
+            error_message.append(_('Some required fields are empty.'))
+
+        unknown = [k for k in data.iterkeys() if k not in mandatory_billing_fields + optional_billing_fields]
+        if unknown:
+            error['common'] = 'Unknown field'
+            error_message.append("Unknown field '%s'" % ','.join(unknown))
+		
+        self.send_mail_note( 'igor.kartashov@setir.es', data.get('name'), data.get('company_name'))
+		
+        return error, error_message
+
+    def send_mail_note ( self, mail_to, contacto, empresa):
+        mail_pool = request.env['mail.mail']
+
+        strBodyHTML = 'Actualzada la empresa:[' + empresa + '] por el contacto:[' + contacto +']'
+
+        values={}
+        values.update({'subject': 'Datos asociado actualizados'})
+        values.update({'email_to': mail_to})
+        values.update({'body_html': strBodyHTML })
+        values.update({'body': 'partner actualizado' })
+		#values.update({'res_id': 'obj.id' }) #[optional] here is the record id, where you want to post that email after sending
+		#values.update({'model': ''Object Name }) #[optional] here is the object(like 'project.project')  to whose record id you want to post that email after sending
+        msg_id = mail_pool.create(values)
+		# And then call send function of the mail.mail,
+        if msg_id:
+		    mail_pool.send([msg_id])
